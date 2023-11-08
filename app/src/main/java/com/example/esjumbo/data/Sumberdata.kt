13a81b329a42ecd4761b03ca77a0d0eecbe00f74
hariwr <hariwr25@gmail.com>
2023-11-08 14:15:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumbo/data/Sumberdata.kt
@@ -0,0 +1,14 @@
+package com.example.esjumbo.data
+
+import com.example.esjumbo.R
+
+object Sumberdata
+{
+    val flavors = listOf(
+    R.string.natural,
+    R.string.vanilla,
+    R.string.lemon,
+    R.string.milk,
+    R.string.lyche
+    )
+}
