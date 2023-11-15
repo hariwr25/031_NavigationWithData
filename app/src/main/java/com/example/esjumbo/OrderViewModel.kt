e27e11ec84d73644c7e64ae06009bd768263655a
hariwr <hariwr25@gmail.com>
2023-11-16 06:35:23 +0700

    Fix Order

--- app/src/main/java/com/example/esjumbo/OrderViewModel.kt
+++ app/src/main/java/com/example/esjumbo/OrderViewModel.kt
@@ -1,40 +1,50 @@
 package com.example.esjumbo
 
-import com.example.esjumbo.data.OrderUIState
 import androidx.lifecycle.ViewModel
+import com.example.esjumbo.data.OrderUIState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import java.text.NumberFormat
 
-private const val HARGA_PER_CUP = 3000
 
-class OrderViewModel : ViewModel() {
+private const val HARGA_PER_CUP =3000
+
+class OrderViewModel : ViewModel(){
     private val _stateUI = MutableStateFlow(OrderUIState())
     val stateUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
-
-    fun setJumlah(jmlEsJumbo: Int) {
+    fun setCustomerDetails(nama: String, nomor: String, alamat: String) {
         _stateUI.update { stateSaatIni ->
             stateSaatIni.copy(
-                jumlah = jmlEsJumbo,
-                harga = hitungHarga(jumlah = jmlEsJumbo)
+                namaPelanggan = nama,
+                nomorTelepon = nomor,
+                alamat = alamat
             )
         }
     }
 
-    fun setRasa(rasaPilihan: String) {
-        _stateUI.update { stateSaatIni -> stateSaatIni.copy(rasa = rasaPilihan) }
-
+    fun setJumlah(jmlEsJumbo:Int){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(jumlah = jmlEsJumbo,
+                harga = hitungHarga(jumlah =jmlEsJumbo))
+        }
     }
 
-    fun resetOrder() {
+    fun setRasa(rasaPilihan: String){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(rasa = rasaPilihan)
+        }
+    }
+    fun resetOrder(){
         _stateUI.value = OrderUIState()
     }
 
-    private fun hitungHarga(jumlah: Int = _stateUI.value.jumlah): String {
-        val kalkulasiHarga = jumlah * HARGA_PER_CUP
+    private fun hitungHarga(
+        jumlah: Int = _stateUI.value.jumlah,
+    ): String {
+        val kalkulasiHarga =jumlah * HARGA_PER_CUP
 
-        return NumberFormat.getInstance().format(kalkulasiHarga)
+        return  NumberFormat.getNumberInstance().format(kalkulasiHarga)
     }
 }
