e1e0f03ea63f91f6e82a96789aea6a9b30133213
hariwr <hariwr25@gmail.com>
2023-11-16 06:39:30 +0700

    done

--- app/src/main/java/com/example/esjumbo/MainActivity.kt
+++ app/src/main/java/com/example/esjumbo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    EsJumboTheme{
+    EsJumboTheme {
         HalamanHome(onNextButtonClicked = {})
     }
 }
